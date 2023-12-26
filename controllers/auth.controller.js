a3124fee1d7eae0271045a5148ba08d887934986
md4648 <124080342+md4648@users.noreply.github.com>
2023-12-26 13:59:03 +0300

    create singup route

---
+++ controllers/auth.controller.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+
+const app = express();
+app.use(express.json()); // Add this line to parse JSON in the request body
+
+export const signup =  async(req, res) => {
+  const { username, email, password } = req.body;
+
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+
+  const newUser = new User({ username, email, password:hashedPassword });
+try{
+await newUser.save();
+  res.status(201).json('User created successfully!');
+}catch(error){
+ res.status(500).json(error.message)
+}
+
+    
+};
--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";// it is used to access env file b/c it is in backend
 import userRouter from "./routes/user.route.js"
+import authRouter from './routes/auth.route.js';
 dotenv.config({ path: '../.env' });
+
  // Load environment variables from .env
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('databse conneted succefully')
@@ -11,10 +13,13 @@ mongoose.connect(process.env.MONGO).then(()=>{
 })
 
 const app = express();
+app.use(express.json())//in order to pass json file to server
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
 });
 
 app.use('/api/user',userRouter)
+app.use('/api/auth', authRouter);
+
 
